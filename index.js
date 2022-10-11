4d67bba2a7c8be045c9b3edc708555fd99daa5db
Luka Vouillemont <luka.vouillemont@corellis.eu>
2022-10-11 20:07:19 +0200

    mobile design completed

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const adviceNumberElt = document.querySelector(".advice-id");
 const adviceElt = document.querySelector(".advice");
 const dividerElt = document.querySelector(".divider");
 const diceElt = document.querySelector(".dice");
+let isFetchingData = false;
 
 const randomIntFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
@@ -12,29 +13,36 @@ const randomIntFromInterval = (min, max) => {
 const randomId = randomIntFromInterval(1, 224);
 
 const makeRequest = async (id, init) => {
-  if (!init) {
-    adviceElt.classList.add("down");
-    adviceNumberElt.classList.add("down");
-    dividerElt.classList.add("down");
+  if (!isFetchingData) {
+    if (!init) {
+      isFetchingData = true;
+      adviceElt.classList.add("down");
+      adviceNumberElt.classList.add("down");
+      dividerElt.classList.add("down");
 
-    const response = await fetch(`${baseUrl}/${id}`);
-    const json = await response.json();
+      const response = await fetch(`${baseUrl}/${id}`);
+      const json = await response.json();
 
-    setTimeout(() => {
+      setTimeout(() => {
+        adviceNumberElt.textContent = `ADVICE #${json.slip.id}`;
+        adviceElt.textContent = `${json.slip.advice}`;
+      }, 1000);
+
+      setTimeout(() => {
+        adviceElt.classList.remove("down");
+        adviceNumberElt.classList.remove("down");
+        dividerElt.classList.remove("down");
+      }, 2000);
+
+      setTimeout(() => {
+        isFetchingData = false;
+      }, 3000);
+    } else {
+      const response = await fetch(`${baseUrl}/${id}`);
+      const json = await response.json();
       adviceNumberElt.textContent = `ADVICE #${json.slip.id}`;
       adviceElt.textContent = `${json.slip.advice}`;
-    }, 1000);
-
-    setTimeout(() => {
-      adviceElt.classList.remove("down");
-      adviceNumberElt.classList.remove("down");
-      dividerElt.classList.remove("down");
-    }, 2000)
-  } else {
-    const response = await fetch(`${baseUrl}/${id}`);
-    const json = await response.json();
-    adviceNumberElt.textContent = `ADVICE #${json.slip.id}`;
-    adviceElt.textContent = `${json.slip.advice}`;
+    }
   }
 };
 
